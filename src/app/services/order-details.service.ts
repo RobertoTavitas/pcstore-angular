03574b7ab3788028c1dc76c1504f3cecc74ceb63
Roberto Tavitas <robertomirandatavitas@gmail.com>
2022-01-14 14:48:29 -0600

    sevices

---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+  pcDetails = [
+    {
+      id: 1,
+      name: "Custom PC model 1",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda!",
+      price: 2000,
+      image: '../../assets/images/pc1.jpg'
+    },
+    {
+      id: 2,
+      name: "Custom PC model 2",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda!",
+      price: 2000,
+      image: '../../assets/images/pc3.jpeg'
+    },
+    {
+      id: 3,
+      name: "Custom PC model 3",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda!",
+      price: 2000,
+      image: '../../assets/images/pc4.jpg'
+    },
+    {
+      id: 4,
+      name: "Custom PC model 4",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda!",
+      price: 2000,
+      image: '../../assets/images/pc5.png'
+    },
+    {
+      id: 5,
+      name: "Custom PC model 5",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda!",
+      price: 2000,
+      image: '../../assets/images/pc6.jpg'
+    },
+    {
+      id: 6,
+      name: "Custom PC model 6",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex delectus ea quidem nihil quia ut, recusandae atque quod laboriosam earum repellat commodi rerum perferendis nulla fuga aliquam quibusdam, veritatis assumenda!",
+      price: 2000,
+      image: '../../assets/images/pc7.jpg'
+    },
+   
+
+  ]
+
+}
